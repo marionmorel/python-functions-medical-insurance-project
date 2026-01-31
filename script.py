7246c9697d9652995ad0bd7934df57aff3d25d72
marionmorel <marion.mrl@live.fr>
2026-01-31 23:00:27 +0000

    Task 8

--- script.py
+++ script.py
@@ -20,13 +20,13 @@ maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0)
 # print("The estimated insurance cost for Maria is " + str(insurance_cost) + " dollars.")
 
 # Initial variables for Omar
-age = 35
-sex = 1 
-bmi = 22.2
-num_of_children = 0
-smoker = 1  
+# age = 35
+# sex = 1 
+# bmi = 22.2
+# num_of_children = 0
+# smoker = 1  
 
 # Estimate Omar's insurance cost 
-insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+omar_insurance_cost = calculate_insurance_cost(35, 1, 22.2, 0, 1)
 
-print("The estimated insurance cost for Omar is " + str(insurance_cost) + " dollars.")
+# print("The estimated insurance cost for Omar is " + str(insurance_cost) + " dollars.")
