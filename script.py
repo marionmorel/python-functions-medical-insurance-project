2ccc137d5f18907d726ed23fa0dda5b53d5c5fc8
marionmorel <marion.mrl@live.fr>
2026-01-31 23:04:12 +0000

    Task 11

--- script.py
+++ script.py
@@ -15,7 +15,7 @@ def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker, name):
 
 # Estimate Maria's insurance cost
 # insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
-maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0)
+maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0, "Maria")
 
 # print("The estimated insurance cost for Maria is " + str(insurance_cost) + " dollars.")
 
@@ -27,6 +27,6 @@ maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0)
 # smoker = 1  
 
 # Estimate Omar's insurance cost 
-omar_insurance_cost = calculate_insurance_cost(35, 1, 22.2, 0, 1)
+omar_insurance_cost = calculate_insurance_cost(35, 1, 22.2, 0, 1, "Omar")
 
 # print("The estimated insurance cost for Omar is " + str(insurance_cost) + " dollars.")
