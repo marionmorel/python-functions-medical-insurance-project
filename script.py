ad477c562b387ea446fb87f193cf6484d86e6813
marionmorel <marion.mrl@live.fr>
2026-01-31 22:51:41 +0000

    Task 4

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
-# Create calculate_insurance_cost() function below: 
-def calculate_insurance_cost():
+# Create calculate_insurance_cost() function below:
+# def calculate_insurance_cost(): 
+def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker):
     estimated_cost = 1000
     print("The estimated insurance cost for this person is " + str(estimated_cost) + " dollards.")
     return estimated_cost
